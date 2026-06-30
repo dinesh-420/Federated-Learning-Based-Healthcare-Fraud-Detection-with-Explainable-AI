0e90767d4ef67117693443c91cced74120d9fa74
dinesh s <dineshsankar4002@gmail.com>
2026-06-30 22:27:19 +0530

    feat: add model evaluation metrics

---
+++ models/evaluate_model.py
@@ -0,0 +1,25 @@
+from sklearn.metrics import (confusion_matrix, classification_report, precision_score, recall_score, f1_score,)
+
+def evaluate_model(model, X_test, y_test):
+    """
+    Evaluate the trained model using multiple metrics.
+    """
+
+    y_pred = model.predict(X_test)
+
+    cm = confusion_matrix(y_test, y_pred)
+    print("Confusion Matrix:")
+    print(cm)
+
+    print("\nClassification Report:")
+    print(classification_report(y_test, y_pred))
+
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    print("\nPrecision:", precision)
+    print("Recall:", recall)
+    print("F1-Score:", f1)
+
+ 
--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 from utils.data_loader import load_dataset
 from preprocessing.preprocess import preprocess_data
 from models.random_forest_model import train_random_forest
+from models.evaluate_model import evaluate_model
 
 DATA_PATH = "data/AI_Based_Medical_Insurance_Claim_Fraud_Detection_Dataset.csv"
 
@@ -11,6 +12,9 @@ def main():
     X_train, X_test, y_train, y_test = preprocess_data(df)
     
     model = train_random_forest(X_train, X_test, y_train, y_test)
-
+    
+    evaluate_model(model, X_test, y_test)
+    
 if __name__ == "__main__":
     main()
+    
